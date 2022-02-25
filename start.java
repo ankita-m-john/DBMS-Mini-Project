63dbc03b041f626b9352ae90d2ee5911adb6b81f
DanTheMan314 <dankoshyg@gmail.com>
2022-02-25 21:49:51 +0530

    login

--- start.java
+++ start.java
@@ -7,7 +7,7 @@ Class.forName("oracle.jdbc.driver.OracleDriver");
   
 //step2 create  the connection object  
 Connection con=DriverManager.getConnection(  
-"jdbc:oracle:thin:@localhost:1521:xe","system","oracle");  
+"jdbc:oracle:thin:@localhost:1521:xe","scott","tiger");  
   
 //step3 create the statement object  
 Statement stmt=con.createStatement();  
